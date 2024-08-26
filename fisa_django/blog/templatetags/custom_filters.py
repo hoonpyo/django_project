9c58523eb3c940a0310d1a7d727a5317f9cf6640
hoonpyo <hp723@naver.com>
2024-08-26 16:20:10 +0900

    django day1 MVT Pattern

---
+++ fisa_django/blog/templatetags/custom_filters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='reverse_string')
+def reverse_string(value):
+    return value[::-1]
+
+# @register.filter(name='reverse_string2')
+# def reverse_string(value):
+#     return value[::-1]
+
+# {% load custom_filters %}
